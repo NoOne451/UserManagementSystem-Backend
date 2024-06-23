0eb1285d27e1f209ec7c8273604f9ba6e68762f9
NoOne451 <ranjithdurai111@gmail.com>
2024-06-23 08:17:55 +0530

    changes

--- controllers/auth.controller.js
+++ controllers/auth.controller.js
@@ -111,8 +111,8 @@ export const verifyOptController = async (req, res) => {
     res.cookie('jwt', token, {
       httpOnly: true, // Prevent access by JavaScript
       secure: true, // Ensure the cookie is sent over HTTPS
-      domain: 'usermanagementsystem.onrender.com', // Make cookie available to all subdomains of onrender.com
-      sameSite: 'Lax', // Allow cookie for same-site requests
+      domain: '.onrender.com', // Make cookie available to all subdomains of onrender.com
+      sameSite: 'none',
       maxAge: 30 * 24 * 60 * 60 * 1000, // 30 days in milliseconds
     });
 
